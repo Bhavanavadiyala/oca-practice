a908b49d151c33b148f1575f62890e72db09573d
bhavanavadiyala <bxv03090@ucmo.edu>
2024-01-30 21:02:10 -0600

    initial commit

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Demo{
+public static void main(String[] args) {
+    System.out.print("Eneter data : ");
+int i = new Scanner(System.in).nextInt();
+    System.out.println("Enetered input : "+i);
+}
+}
